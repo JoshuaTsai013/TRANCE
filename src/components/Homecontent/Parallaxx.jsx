16f18900443d5a3310cbd915bdce2c20a1dbdf25
ss23094527 <ss23094527@gmail.com>
2023-03-05 23:42:06 +0800

    fix-layout1.3

--- src/components/Homecontent/Parallaxx.jsx
+++ src/components/Homecontent/Parallaxx.jsx
@@ -24,15 +24,16 @@ function Parallaxx() {
         <ParallaxLayer offset={0} speed={-0.4}>
           <div class="animation_layer parallax" id="jungle1"></div>
         </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={-0.7}>
+          <div class="animation_layer parallax" id="jungle4"></div>
+        </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.35}>
           <div class="animation_layer parallax" id="jungle2"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={-0.1}>
           <div class="animation_layer parallax" id="jungle3"></div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={-0.1}>
-          <div class="animation_layer parallax" id="jungle4"></div>
-        </ParallaxLayer>
+       
         <ParallaxLayer offset={0} speed={0.2}>
           <div class="animation_layer parallax" id="manonmountain"></div>
         </ParallaxLayer>

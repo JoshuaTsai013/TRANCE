4e6c9e5e672a3da9bc30e2b5a756740a0e0b6342
ss23094527 <ss23094527@gmail.com>
2023-03-07 11:01:57 +0800

    fix-layout1.4

--- src/components/Homecontent/Whatdo.jsx
+++ src/components/Homecontent/Whatdo.jsx
@@ -2,8 +2,9 @@ import React from "react"
 
 import { Canvas } from '@react-three/fiber'
 import { PresentationControls, Stage, useGLTF } from "@react-three/drei";
-
+import { Button, Space } from 'antd';
 import './styles/whatdo.css';
+
 
 
 
@@ -17,10 +18,20 @@ function Whatdo() {
   return (
     <div className="whatdo-container">
 
-      <h1 className="whatdo-title">➥ What do we do?</h1>
-    
 
-      <Canvas dpr={[1, 2]} shadows camera={{ fov:20 , position: [5, 5, 10]}} style={{ "position": "absolute",width: "100%", height: "100%" ,marginTop:"-5%"}}>
+      <div className="whatdo-content">
+        <h1 className="whatdo-title">➥ What do we do?</h1>
+        <h3>This is a world with everything</h3>
+
+        <Space direction="vertical" style={{ width: '10%' }}>
+    <Button style={{ paddingBottom: '25%' }} type="primary" block>
+      DISCOVER
+    </Button>
+    </Space >
+
+      </div>
+
+      <Canvas dpr={[1, 2]} shadows camera={{ fov: 20, position: [5, 5, 10] }} style={{ "position": "absolute", width: "100%", height: "100%", marginTop: "-5%" }}>
 
         <PresentationControls speed={1.5} global zoom={.5} polar={[-1.3, Math.PI / 6]}>
           <Stage environment={"sunset"}>

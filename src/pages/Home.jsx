08ae9a69f1e825298c8d470227819e62d839e6a8
Joshuatsai <joshuatsai1213@gmail.com>
2023-03-15 01:17:29 +0800

    try.fix.deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Header from "../components/Header";
-import HomeContent from "../components/HomeContent/HomeContent";
+import HomeContent from "../components/HomeContent";
 
 function Home() {
     return (

fdd2e3c9be51858d1509e91c79d44ee3766a4dae
ss23094527 <ss23094527@gmail.com>
2023-04-08 22:26:26 +0800

    Done Skills component

--- src/components/HomeContent/Skills.jsx
+++ src/components/HomeContent/Skills.jsx
@@ -1,18 +1,20 @@
 
 import Col from "react-bootstrap/esm/Col";
-import{Container,Row} from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import meter1 from "../../../public/images/meter1.svg";
-import meter2 from "../../../public/images/meter2.svg";
-import meter3 from "../../../public/images/meter3.svg";
+
 import './styles/skills.css';
+import CountUp from 'react-countup';
+import { useState } from "react";
+import ScrollTrigger from 'react-scroll-trigger';
 
 export const Skills = () => {
 
     const responsive = {
         superLargeDesktop: {
-            
+
             breakpoint: { max: 4000, min: 3000 },
             items: 5
         },
@@ -30,6 +32,7 @@ export const Skills = () => {
         }
     };
 
+    const [couterOn, setCouterOn] = useState(false);
 
 
     return (
@@ -41,21 +44,35 @@ export const Skills = () => {
                             <h2>
                                 Skills
                             </h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia laudantium asperiores inventore, <br />quibusdam enim dolorem aperiam porro corrupti nobis sit dignissimos facilis. Reiciendis numquam voluptatibus dolor. Omnis, dolores! Blanditiis, natus!</p>
+                            <p>This is all the skills we possess.</p>
                             <Carousel responsive={responsive} infinite={true} className="skill-slider">
                                 <div className="item">
+                                <ScrollTrigger onEnter={() => setCouterOn(true)} onExit={() => setCouterOn(false)}>
+                                       <CountUp start={0} end={85} className="number1" duration={7} suffix="%" delay={8} />
+                                    </ScrollTrigger>
                                     <img src={meter1} alt="Image" />
+                                   
                                     <h5>Music production</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={meter2} alt="Image" />
+                                    <ScrollTrigger onEnter={() => setCouterOn(true)} onExit={() => setCouterOn(false)}>
+                                       <CountUp start={0} end={90} className="number2" duration={7} suffix="%" delay={8} />
+                                    </ScrollTrigger>
+                                    <img src={meter1} alt="Image" />
                                     <h5>3D modeling</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={meter3} alt="Image" />
+                                <ScrollTrigger onEnter={() => setCouterOn(true)} onExit={() => setCouterOn(false)}>
+                                       <CountUp start={0} end={95} className="number3" duration={7} suffix="%" delay={8} />
+                                    </ScrollTrigger>
+                                    <img src={meter1} alt="Image" />
                                     <h5>Web Development</h5>
                                 </div>
                                 <div className="item">
+                                <ScrollTrigger onEnter={() => setCouterOn(true)} onExit={() => setCouterOn(false)}>
+                                       <CountUp start={0} end={95} className="number4" duration={7} suffix="%" delay={8} />
+                                    </ScrollTrigger>
+
                                     <img src={meter1} alt="Image" />
                                     <h5>Graphic design</h5>
                                 </div>

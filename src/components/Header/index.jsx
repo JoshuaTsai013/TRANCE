ad34737b0a178e20311583213d8dcd91c3d4beae
ss23094527 <ss23094527@gmail.com>
2023-04-11 02:49:18 +0800

    ADD SIGN

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,10 +10,33 @@ import styles from './Headerstyle.module.css';
 // import '../pages/Home';
 // import Darklightmode from '../Darklightmode';
 import { NavLink } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 // import { SearchOutlined } from '@ant-design/icons';
+import { createClient } from "@supabase/supabase-js";
 
+const supabase = createClient('https://fzzbffjgesbywijwlztg.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6emJmZmpnZXNieXdpandsenRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODExMTg0ODQsImV4cCI6MTk5NjY5NDQ4NH0.CUYU0u1DTvOYm3jYZhKZ690cwOYFEAn66Y9fh7H-NqI');
 
 function Header() {
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    let aa = !session ? "s" : session.user
+
+    console.log(aa)
+  }, [])
+
+  const [session, setSession] = useState(null)
+
+  async function logout() {
+    const { error } = await supabase.auth.signOut()
+}
 
 
   return (
@@ -58,18 +81,29 @@ function Header() {
                   />
                   <Button className='rounded-pill' variant="outline-light"><SearchOutlined /></Button>
                 </Form> */}
+                <div className={styles.login}>
 
-                {/* <Nav className="me-3 gap-2 login">
-                  <Nav.Link className="btn btn-outline-light text-white" href="#">LOGIN</Nav.Link>
-                  <Nav.Link eventKey={2} className="btn btn-outline-light text-white " href="#">
-                    SIGN UP
-                  </Nav.Link>
-                </Nav> */}
+                
+         <Nav className="me-3 gap-2 login">
+                    <Nav.Link as={NavLink} to="/Sign" className="btn btn-outline-light text-white" href="#">
+                      {!session ? <div>
+                        <h3>SIGN UP/IN</h3> 
+                         </div> :
+                          <div> <h3>{session.user.email}</h3>
+                          <Button onClick={logout}>  <h3>signOut</h3></Button>
+                          </div>
+                          }
+                    </Nav.Link>
+
+
+                  </Nav> 
+                </div>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
           </Container>
-        </Navbar>
-      ))}
+        </Navbar >
+      ))
+      }
     </>
   );
 }

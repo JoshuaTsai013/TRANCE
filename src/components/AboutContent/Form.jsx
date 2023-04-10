882704e5d9088d65c22676ee9faa656eea838295
Joshuatsai <joshuatsai1213@gmail.com>
2023-04-10 19:33:19 +0800

    about textV1

--- src/components/AboutContent/Form.jsx
+++ src/components/AboutContent/Form.jsx
@@ -1,28 +1,45 @@
 import React, { useState } from 'react';
 import './styles/Form.css'
 function Form() {
+  const [mail, setMail] = useState('');
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
+  const [message, setMessage] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(`Name: ${name}, Phone: ${phone}`);
+    console.log(`Gmail: ${mail},Name: ${name}, Phone: ${phone}`);
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input type="text" value={name} onChange={e => setName(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Phone:
-        <input type="tel" value={phone} onChange={e => setPhone(e.target.value)} />
-      </label>
-      <br />
-      <input type="submit" value="Submit" />
-    </form>
+    <div className="formFrame">
+      <form onSubmit={handleSubmit}>
+        <label className="formLong">
+          Gmail:
+          <input type="text" value={mail}  size="35" onChange={e => setMail(e.target.value)} />
+        </label>
+
+
+        <label className="formShort">
+          Name:
+          <input type="text" value={name} size="16" onChange={e => setName(e.target.value)} />
+        </label>
+
+        <label className="formShort">
+          Phone:
+          <input type="tel" value={phone} size="17" onChange={e => setPhone(e.target.value)} />
+        </label>
+
+        <label for="formMessage"></label>
+          Message:
+          <textarea id="formMessage" name="w3review" rows="4" cols="40">
+            write us something
+          </textarea>        
+          
+
+        {/* <input type="submit" value="Submit" className="" /> */}
+      </form>
+    </div>
   );
 }
 
--- src/components/AboutContent/Parallaxx.jsx
+++ src/components/AboutContent/Parallaxx.jsx
@@ -18,7 +18,7 @@ function Parallaxx() {
         </div>
       </ParallaxLayer>
       <ParallaxLayer offset={0.7} speed={0.2} className="animation">{/* sticky={{ start: 1, end: 3 }} */}
-        <div className="aboutText" style={{ right: '10%' }}>
+        <div className="aboutText" style={{ right: '5%' }}>
           &#8251; Trance is a music genre that emerged in the late 1980s and early 1990s <br />
           &#8251; It is characterized by fast tempo, repetitive melodies, and uplifting or hypnotic atmospheres <br />
           &#8251; Trance music often incorporates elements from other genres such as techno, house, ambient, and classical <br />
@@ -34,8 +34,8 @@ function Parallaxx() {
           <h1>US</h1>
         </div>
       </ParallaxLayer>
-      <ParallaxLayer offset={1.8} speed={0.2} className="animation">{/* sticky={{ start: 1, end: 3 }} */}
-        <div className="aboutText" style={{ left: '10%' }}>
+      <ParallaxLayer offset={1.7} speed={0.2} className="animation">{/* sticky={{ start: 1, end: 3 }} */}
+        <div className="aboutText" style={{ left: '5%' }}>
           We are a group of talented artists who made graphic design and music, we are TRANCE.<br />
           We love to create beautiful and inspiring artworks for various projects and clients.<br />
           We have skills in different tools and genres, such as Photoshop, Illustrator, Pro Tools, Ableton Live, etc.<br />
@@ -53,7 +53,7 @@ function Parallaxx() {
       </ParallaxLayer>
 
       <ParallaxLayer offset={2.9} speed={0.2} className="animation">{/* sticky={{ start: 1, end: 3 }} */}
-        <div className="aboutText" style={{ right: '10%' }}>
+        <div className="aboutText" style={{ right: '5%' }}>
           Hi, I'm a musician and a graphic designer who loves to create and share my art with the world.<br />
           I am a compose and I love playing guitar, I compose songs in different genres and styles.<br />
           I also design logos, posters, flyers and websites for fun.<br />
@@ -71,8 +71,8 @@ function Parallaxx() {
         </div>
       </ParallaxLayer>
 
-      <ParallaxLayer offset={4.1} speed={0.2} className="animation">{/* sticky={{ start: 1, end: 3 }} */}
-        <div className="aboutText" style={{ right: '10%' }}>
+      <ParallaxLayer offset={4} speed={0.2} className="animation">{/* sticky={{ start: 1, end: 3 }} */}
+        <div className="aboutText" style={{ right: '5%' }}>
           - Hi, I'm a talented graphic designer, creator and artist.<br />
           - I love to design logos, posters, flyers, websites and more.<br />
           - I have a passion for creativity and innovation.<br />
@@ -85,6 +85,7 @@ function Parallaxx() {
       <ParallaxLayer offset={4.6} factor={1.2} style={{ top: '0' }}>
         <div className="animation_layer aboutback"></div>
       </ParallaxLayer>
+
       <ParallaxLayer offset={5} speed={0.4} className="animation">
         <div className="title" style={{ left: '10%' }}>
           <h1>CONTACT</h1>
@@ -92,11 +93,9 @@ function Parallaxx() {
         </div>
       </ParallaxLayer>
 
-      <ParallaxLayer offset={5} speed={0} className="animation">
-        <Form style={{ right: '10%' }} />
+      <ParallaxLayer offset={4.9} speed={0} className="animation">
+        <Form />
       </ParallaxLayer>
-
-
 
       <ParallaxLayer offset={5.8} factor={0.45} style={{ bottom: '0' }}>
         <Footer />

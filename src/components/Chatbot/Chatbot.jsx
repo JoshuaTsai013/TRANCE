d1a3fde9a0e082a2a5be476fb9970a2746686d5b
ss23094527 <ss23094527@gmail.com>
2023-05-28 21:54:39 +0800

    修改機器人

--- src/components/Chatbot/Chatbot.jsx
+++ src/components/Chatbot/Chatbot.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Input, Button } from 'antd';
 import { SmileOutlined, SendOutlined } from '@ant-design/icons';
 import './Chatbot.css';
+import { doc, collection, addDoc } from "firebase/firestore";
+import { db } from "../../firebase";
 
 const Chatbot = () => {
   const [messages, setMessages] = useState([]);
@@ -9,6 +11,9 @@ const Chatbot = () => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   const botResponses = {
+    哈囉: '哈囉!若需要幫助請打"幫助"',
+    我愛你: '謝謝，我也愛您^^',
+    你是誰: '我是客服機器人!為您解決所有的疑難雜症"',
     幫助: '我是TRANCE專屬客服機器人!\n有關3D模型相關學習網站，請輸入"Model"\n有關Trance音樂，請輸入"Music"\n如需合作請輸入"Work"',
     Model: '3D模型網站:https://sketchfab.com/feed 超全blender素材網站:https://blenderco.cn/category/yuanma?o=recommend Blender學習:https://kaicho-cg.com/study/',
     Music: '[tracne音樂]',
@@ -19,20 +24,31 @@ const Chatbot = () => {
     setInputValue(e.target.value);
   };
 
-  const handleSendMessage = () => {
+  const handleSendMessage = async () => {
     if (!inputValue) {
       return;
     }
-
+  
     const newMessage = { text: inputValue, isUser: true };
     setMessages((prevMessages) => [...prevMessages, newMessage]);
-
+  
+    try {
+      const messageRef = collection(db, "usermessages");
+      const newDoc = await addDoc(messageRef, {
+        text: inputValue,
+        isUser: true,
+      });
+      console.log("Message added with ID: ", newDoc.id);
+    } catch (error) {
+      console.error("Error adding message: ", error);
+    }
+  
     setTimeout(() => {
-      const botResponse = { text: botResponses[inputValue] || '', isUser: false };
+      const botResponse = { text: botResponses[inputValue] || "", isUser: false };
       setMessages((prevMessages) => [...prevMessages, botResponse]);
     }, 1000);
-
-    setInputValue('');
+  
+    setInputValue("");
   };
 
   useEffect(() => {

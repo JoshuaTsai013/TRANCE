3f43984a801fa675a0b3d3a36f95d899fab754ef
Joshuatsai <joshuatsai1213@gmail.com>
2023-05-29 15:39:27 +0800

    music temp

--- src/components/MusicContent/index.jsx
+++ src/components/MusicContent/index.jsx
@@ -5,14 +5,25 @@ import Footer from '../../components/Footer'
 import CarouselImage1 from '../../assets/images/Cimage1.png'
 import CarouselImage2 from '../../assets/images/Cimage2.png'
 import CarouselImage3 from '../../assets/images/Cimage3.png'
+import Logo from '../../assets/images/LOGO.png'
+import SongBlock from './SongBlock'
 
 import { CCarousel, CCarouselItem, CImage } from '@coreui/react'
-
+import { HomeFilled, SearchOutlined, HeartFilled } from '@ant-design/icons';
 
 function MusicContent() {
+
     return (
         <div id="musicLayout">
-            <div id="musicsideBar"></div>
+            <div id="musicsideBar">
+                {/* icon */}
+                <img src={Logo} style={{ width: "100px",marginTop: "50px",marginBottom: "60px" }} alt="CD" /> 
+                {/* list */}
+                <div className="listFrame"><HomeFilled style={{ fontSize: '25px' , color: '#efeef7' }}/><p className="sidebarListText">H O M E</p></div>
+                <div className="listFrame"><SearchOutlined style={{ fontSize: '25px' , color: '#efeef7' }}/><p className="sidebarListText">S E A R C H</p></div>
+                <div className="listFrame"><HeartFilled style={{ fontSize: '25px', color: '#efeef7'  }}/><p className="sidebarListText">L I K E</p></div>
+
+            </div>
             <div id="musicContentArea">
                 <div className="musicBannerFrame">
                     <CCarousel controls indicators transition="crossfade">
@@ -27,19 +38,31 @@ function MusicContent() {
                         </CCarouselItem>
                     </CCarousel>
                 </div>
+
+                <p className="musicTitle">TOP 10</p>
                 <Row style={{ width: "100%", paddingLeft: "12px", paddingRight: "12px", paddingBottom: "12px", margin: "0" }}>
-                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><div className="musicItem"></div></Col>
-                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><div className="musicItem"></div></Col>
-                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><div className="musicItem"></div></Col>
-                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><div className="musicItem"></div></Col>
-                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><div className="musicItem"></div></Col>
-                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><div className="musicItem"></div></Col>
-                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><div className="musicItem"></div></Col>
-                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><div className="musicItem"></div></Col>
-                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><div className="musicItem"></div></Col>
-                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><div className="musicItem"></div></Col>
-                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><div className="musicItem"></div></Col>
-                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><div className="musicItem"></div></Col>
+                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><SongBlock/><h2 style={{ marginTop: "2px",marginBottom: "0",color: "#efeef7"}} >bbb</h2><p style={{ color: "#efeef7"}}>aaa</p></Col>
+                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><SongBlock/></Col>
+                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><SongBlock/></Col>
+                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><SongBlock/></Col>
+                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><SongBlock/></Col>
+                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><SongBlock/></Col>
+                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><SongBlock/></Col>
+                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><SongBlock/></Col>
+                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><SongBlock/></Col>
+                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><SongBlock/></Col>
+                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><SongBlock/></Col>
+                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><SongBlock/></Col>
+                </Row>
+
+                <p className="musicTitle">MUSIC TYPE</p>
+                <Row style={{ width: "100%", paddingLeft: "12px", paddingRight: "12px", paddingBottom: "12px", margin: "0" }}>
+                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><SongBlock/></Col>
+                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><SongBlock/></Col>
+                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><SongBlock/></Col>
+                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><SongBlock/></Col>
+                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><SongBlock/></Col>
+                    <Col sm={12} md={6} lg={4} xl={2} className="musicItemFrame"><SongBlock/></Col>
                 </Row>
 
                 <div style={{ height: "360px" }}><Footer /></div>
